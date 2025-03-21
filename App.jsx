6b25ee1932dfe3e65549d6f621c7e4f92b08a7e3
Anirban Chandra <136344832+anirban-chandra@users.noreply.github.com>
2025-03-21 18:32:42 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react"
+
+function App() {
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    fetch("https://sum-server.100xdevs.com/todos")
+      .then(async (res) => {
+        const json = await res.json();
+        setTodos(json.todos);
+      })
+  }, [])
+
+  return (
+    <div>
+      {todos.map(({title, description}) => <Todo title={title} description={description} />)}
+    </div>
+  )
+}
+
+function Todo({title, description}) {
+  return <div>
+    <h2>
+      {title}
+    </h2>
+    <h5>
+      {description}
+    </h5>
+  </div>
+}
+
+export default App
